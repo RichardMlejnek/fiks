ae7f0cf200e22672b7dedf3f9ef2b9a531cd15b6
Richard-Mlejnek <125026983+game-translations@users.noreply.github.com>
2023-10-02 09:51:39 +0200

    Oprava programu

--- kabely.py
+++ kabely.py
@@ -6,7 +6,7 @@ class Kabely:
 		if nacti_ze_vstupu:
 			self.nactiZeVstupu()
 		else:
-			self.nactiZeSouboru("C:/Users/Riky/Documents/AutoHotkey/input.txt")
+			self.nactiZeSouboru("C:/Users/Riky/Downloads/input.txt")
 
 		# print(zadani)
 		# print(zarizeni)
@@ -27,13 +27,15 @@ class Kabely:
 					ajajaj = True
 					continue
 				elif len(self.zarizeni[i][nazev_zarizeni]) > 2:
-					raise "Zarizeni je tu mockrat - " + nazev_zarizeni + ", Cyklus - " + i
+					raise Exception("Zarizeni je tu mockrat - " + nazev_zarizeni + ", Cyklus - " + str(i + 1))
 
 				c1, c2 = self.zarizeni[i][nazev_zarizeni]
-				self.jeOkrajVolny(podlaha, c1, c2)
+				if not self.jeOkrajVolny(podlaha, c1, c2):
+					ajajaj = True
+					break
 
-			for p in podlaha:
-				print(p)
+			# for p in podlaha:
+			# 	print(p)
 
 			if ajajaj:
 				self.odpovedi.append("ajajaj")
@@ -53,18 +55,15 @@ class Kabely:
 		for cyklus in range(2):
 			poz_x, poz_y = sour1
 			zmena_x, zmena_y = 0, 0
-			koeficient = 0
 
 			if poz_x <= 0:
-				zmena_y = 1
-			elif poz_x >= max_delka:
-				zmena_y = -1
+				zmena_y = -1 if druhy_smer else 1
+			elif poz_x >= max_delka - 1:
+				zmena_y = 1 if druhy_smer else -1
 			elif poz_y <= 0:
-				zmena_x = -1
-				koeficient = -1
-			elif poz_y >= max_sirka:
-				zmena_x = 1
-				koeficient = -1
+				zmena_x = 1 if druhy_smer else -1
+			elif poz_y >= max_sirka - 1:
+				zmena_x = -1 if druhy_smer else 1
 
 			koliduje = []
 			je_dvakrat = []  # Zařízení které jsou mezi hledanými zařízeními dvakrát
@@ -73,7 +72,7 @@ class Kabely:
 					lze_projit = True
 					break
 
-				if pole[poz_x][poz_y] is not False and pole[poz_x][poz_y] != pole[sour2[0]][sour2[1]]:
+				if pole[poz_x][poz_y] is not False and pole[poz_x][poz_y] != pole[sour1[0]][sour1[1]]:
 					# Když jsou v trase dva stejne lze je obejit
 					if pole[poz_x][poz_y] in koliduje:
 						koliduje.remove(pole[poz_x][poz_y])
@@ -81,40 +80,43 @@ class Kabely:
 					else:
 						koliduje.append(pole[poz_x][poz_y])
 
-				if poz_x <= 0 and poz_y <= 0:
+				if poz_x in [0, max_delka - 1] and poz_y in [0, max_sirka - 1]:
 					zmena_x = 0
-					zmena_y = 1
-					koeficient = 0
+					zmena_y = 0
+
+				if poz_x <= 0 and poz_y <= 0:
+					if not druhy_smer:
+						zmena_y = 1
+					else:
+						zmena_x = 1
 
 				if poz_x <= 0 and poz_y >= max_sirka - 1:
-					zmena_x = 1
-					zmena_y = 0
-					koeficient = -1
+					if not druhy_smer:
+						zmena_x = 1
+					else:
+						zmena_y = -1
 
 				if poz_x >= max_delka - 1 and poz_y >= max_sirka - 1:
-					zmena_x = 0
-					zmena_y = -1
-					koeficient = 0
+					if not druhy_smer:
+						zmena_y = -1
+					else:
+						zmena_x = -1
 
 				if poz_x >= max_delka - 1 and poz_y <= 0:
-					zmena_x = -1
-					zmena_y = 0
-					koeficient = -1
+					if not druhy_smer:
+						zmena_x = -1
+					else:
+						zmena_y = 1
 
-				# Prohozeni smeru a pridani koeficientu
-				if druhy_smer:
-					poz_x += zmena_y * koeficient
-					poz_y += zmena_x * koeficient
-				else:
-					poz_x += zmena_x
-					poz_y += zmena_y
+				poz_x += zmena_x
+				poz_y += zmena_y
 
 			if lze_projit and len(koliduje) > 0:
 				lze_projit = False
 
 			if lze_projit:
 				break
-			else:
+			elif not druhy_smer:
 				druhy_smer = True
 
 		return lze_projit
@@ -138,22 +140,24 @@ class Kabely:
 	def nactiZeSouboru(self, nazev_souboru):
 		soubor = open(nazev_souboru, "r")
 
-		pocet_zadani = int(soubor.readline())
+		pocet_zadani = int(soubor.readline().rstrip('\n'))
 
 		# Načtení dat do polí
 		self.zadani = []
 		self.zarizeni = []
 		for i in range(pocet_zadani):
-			self.zadani.append([int(inpt) for inpt in soubor.readline().split(" ")])
+			self.zadani.append([int(inpt) for inpt in soubor.readline().rstrip('\n').split(" ")])
 			zarizeni_informace = {}
 			for i2 in range(self.zadani[-1][2]):
-				y, x, c = soubor.readline().split(" ")
+				y, x, c = soubor.readline().rstrip('\n').split(" ")
+
 				if c not in zarizeni_informace.keys():
 					zarizeni_informace[c] = []
+
 				zarizeni_informace[c].append([int(y), int(x)])
 			self.zarizeni.append(zarizeni_informace)
 
 		soubor.close()
 
 
-Kabely(True)
+Kabely(False)
